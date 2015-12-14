ef4b60b8f7e34db1ec35509afe7bacf2456dbd9e
yavnodm@gmail.com <yavnodm@gmail.com>
2015-12-15 00:51:46 +0300

    Если файл не найден, должно возвращаться 404

--- app/controllers/external_page_contents_controller.rb
+++ app/controllers/external_page_contents_controller.rb
@@ -4,6 +4,7 @@ class ExternalPageContentsController < ApplicationController
 
     @content_item = ExternalPageContent.find_by_id(params[:id])
     return render(layout:false, status: 404) if @content_item.nil?
+    return render(layout:false, status: 404) unless File.exist?(@content_item.path)
 
     @content = File.read(@content_item.path)
 
