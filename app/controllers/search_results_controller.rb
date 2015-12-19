3bff2676293386d7b6194b9f5fe4c461c46386ed
yavnodm@gmail.com <yavnodm@gmail.com>
2015-12-19 20:08:24 +0300

    начальная версия вестки на паджинатор

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,5 +1,13 @@
 class SearchResultsController < ApplicationController
 
+  layout 'layouts/library.haml', :only => [:index]
+
+  before_action :set_body_class
+
+  def set_body_class
+    @body_class = 'bluegamma'
+  end
+
   def index
 
     @result = Page.sphinx_search(params)
