305b79bf18be764765625873eec62edc765e29bd
yavnodm@gmail.com <yavnodm@gmail.com>
2016-02-19 10:44:53 +0300

    мейлер теперь читает параметры из конфига

--- app/mailers/doctrine_mailer.rb
+++ app/mailers/doctrine_mailer.rb
@@ -6,17 +6,17 @@ class DoctrineMailer < ApplicationMailer
   #   en.doctrine_mailer.new_feedback.subject
   #
 
-  def create_delivery_options
+  def create_delivery_options(yaml_object)
 
     options = {
-        address:              'smtp.gmail.com',
-        port:                 587,
-        domain:               'gmail.com',
-        user_name:            '__user__@gmail.com',
-        password:             '__password__',
-        authentication:       :login,
-        enable_starttls_auto: true,
-        delivery_method:      :smto
+        address:              yaml_object['address'],
+        port:                 yaml_object['port'].to_i,
+        domain:               yaml_object['domain'],
+        user_name:            yaml_object['user_name'],
+        password:             yaml_object['password'],
+        authentication:       yaml_object['authentication'],
+        enable_starttls_auto: (yaml_object.has_key? 'enable_starttls_auto' and yaml_object['enable_starttls_auto'] == 'true'),
+        delivery_method:      yaml_object['delivery_method']
     }
 
     options
@@ -27,9 +27,11 @@ class DoctrineMailer < ApplicationMailer
   def new_feedback(feedback_entry)
     @feedback_entry = feedback_entry
 
-    mail from: '__user__@gmail.com',
-         to: "__target_user__@gmail.com",
+    yaml_object = YAML.load(File.read(File.join(Rails.root, 'config', 'mail_settings.yml')))
+
+    mail from: yaml_object['from'],
+         to: yaml_object['to'],
          subject: 'new feedback',
-         delivery_method_options: create_delivery_options
+         delivery_method_options: create_delivery_options(yaml_object)
   end
 end
