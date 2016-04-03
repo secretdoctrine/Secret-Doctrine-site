fe9b322a0f5bdcc96201cd9a81b2e64fafa14426
Helga <iougai.v@gmail.com>
2016-04-03 18:42:46 +0300

    Скорректирован текст для копирования цитат

--- vendor/extensions/books/app/models/refinery/books/external_page_content.rb
+++ vendor/extensions/books/app/models/refinery/books/external_page_content.rb
@@ -2,7 +2,7 @@ module Refinery
   module Books
     class ExternalPageContent < Refinery::Core::BaseModel
 
-      belongs_to :page
+      belongs_to :book_page
 
       PDF_TYPE = 0
       HTML_TYPE = 1
