e97326ec8e9401da9e8d088f8064f60f4873e31b
Shruf <yavnodm@gmail.com>
2017-08-14 07:12:49 -0700

    теперь должны применяться изменения в обложке книги

--- vendor/extensions/books/app/controllers/refinery/books/admin/books_controller.rb
+++ vendor/extensions/books/app/controllers/refinery/books/admin/books_controller.rb
@@ -111,7 +111,7 @@ module Refinery
 
         # Only allow a trusted parameter "white list" through.
         def book_params
-          params.require(:book).permit(:name, :name_prefix, :tree_prefix, :name_comment, :order_number, :picture_path_id,
+          params.require(:book).permit(:name, :name_prefix, :tree_prefix, :name_comment, :order_number, :cover_picture_id,
                                        :synopsis, :year, :author, :page_count, :can_buy, :book_category_id, :page_format,
                                        :taggable_name)
         end
